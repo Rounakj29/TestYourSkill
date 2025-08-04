c7393d6b23ca3da0b12a4b9b77370fba84708e34
Rounak Jaiswal <Rounak.Jaiswal@insight.com>
2025-08-04 18:30:57 +0530

    chore: upgrade Angular dependencies to version 19.x and update related packages
    - Updated Angular packages from version 18.2.x to 19.2.x in package.json.
    - Changed import for CommonEngine from '@angular/ssr' to '@angular/ssr/node' in server.ts.
    - Adjusted formatting in app.component.ts and skillverify.component.ts for consistency.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,11 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { SkillverifyComponent } from "./skillverify/skillverify.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, SkillverifyComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, SkillverifyComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend';
--- frontend/src/app/skillverify/skillverify.component.ts
+++ frontend/src/app/skillverify/skillverify.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-skillverify',
-  standalone: true,
-  imports: [],
-  templateUrl: './skillverify.component.html',
-  styleUrl: './skillverify.component.css'
+    selector: 'app-skillverify',
+    imports: [],
+    templateUrl: './skillverify.component.html',
+    styleUrl: './skillverify.component.css'
 })
 export class SkillverifyComponent {
 
